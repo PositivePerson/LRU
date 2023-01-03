bbb0386a72c779f708304d192602fb8b483870d9
Bartek <wonderfulLifeVision@gmail.com>
2023-01-03 15:17:59 +0100

    refactor: rebuilded to LFU (shortened code)

--- findLeast.h
+++ findLeast.h
@@ -20,48 +20,53 @@ int findArrayMinimum(int arr[]) {
 }
 
 // Find which from "storage" number to replace
-int findLeast(vector<pair<int, int>> &frequencies, vector<int> pages, vector<int> &storage, vector<pair<int, bool>> removedPages) {
-    cout << "--------- findLeast ---------\n";
-    int countLeasts = 1;
+int findLeast( vector<int> pages, vector<int> &storage, vector<int> storageOrder) {
 
-    int storageFreq[storage.size()];
-    for(int j=0 ; j<storage.size() ; j++) {
-        for (int i = 0; i < frequencies.size(); i++) {
-            if (frequencies[i].first == storage[j]) {
-                storageFreq[j] = frequencies[i].second;
-                cout << "Filling in storageFreq[" << j << "] with value " << frequencies[i].second << '\n';
-            }
-        }
+    for(int i=0 ; i<storageOrder.size() ; i++) {
+        if(storageOrder[i] == 0) return storage[i];
     }
-    int min = findArrayMinimum(storageFreq);
-    cout << "Minimum freq in storage is: " << min << '\n';
-    for(int i=1 ; i<storage.size(); i++) { // Through storageFreq[]
-        if(storageFreq[i] == min) {
-            cout << "(inside findLeast:) storageFreq[i] == storageFreq[i-1] == " << storageFreq[i] << ")\n";
-            countLeasts++;
-        }
-//        else {
-//            break; // To avoid error in case (frequencies looks like): 1, 1, 2, 2, 2
+    return -999;
+//    cout << "--------- findLeastRecently ---------\n";
+//    int countLeasts = 1;
+//
+//    int storageFreq[storage.size()];
+//    for(int j=0 ; j<storage.size() ; j++) {
+//        for (int i = 0; i < frequencies.size(); i++) {
+//            if (frequencies[i].first == storage[j]) {
+//                storageFreq[j] = frequencies[i].second;
+//                cout << "Filling in storageFreq[" << j << "] with value " << frequencies[i].second << '\n';
+//            }
 //        }
-    }
-
-    if(countLeasts == 1) {
-        cout << "Returning countLeasts == 1\n";
-        cout << "------ end of findLeast ------\n";
-        return frequencies[0].first;
-    } else {
-        // find which page (from storage) came first
-        for(int k=0 ; k<pages.size() ; k++){
-//            cout << "(inside findLeast:) Error: Moved out of for loop. Page " << pages[k] << " \n";
-            for(int i=0 ; i<storage.size() ; i++) {
-                if( storageFreq[i] == min && removedPages[k].first == storage[i] && removedPages[k].second == false) {
-                    cout << "(inside findLeast:) Im returning page " << pages[k] << '\n';
-                    cout << "------ end of findLeast ------\n";
-                    return pages[k];
-                }
-            }
-        }
-    }
+//    }
+//    int min = findArrayMinimum(storageFreq);
+//    cout << "Minimum freq in storage is: " << min << '\n';
+//    for(int i=1 ; i<storage.size(); i++) { // Through storageFreq[]
+//        if(storageFreq[i] == min) {
+//            cout << "(inside findLeast:) storageFreq[i] == storageFreq[i-1] == " << storageFreq[i] << ")\n";
+//            countLeasts++;
+//        }
+////        else {
+////            break; // To avoid error in case (frequencies looks like): 1, 1, 2, 2, 2
+////        }
+//    }
+//
+//    if(countLeasts == 1) {
+//        cout << "Returning countLeasts == 1\n";
+//        cout << "------ end of findLeastRecently ------\n";
+//        return frequencies[0].first;
+//    } else {
+//        // find which page (from storage) came first
+//        for(int k=0 ; k<pages.size() ; k++){
+////            cout << "(inside findLeast:) Error: Moved out of for loop. Page " << pages[k] << " \n";
+//            for(int i=0 ; i<storage.size() ; i++) {
+//                if( storageFreq[i] == min && removedPages[k].first == storage[i] && removedPages[k].second == false) {
+//                    cout << "(inside findLeast:) Im returning page " << pages[k] << '\n';
+//                    cout << "------ end of findLeastRecently ------\n";
+//                    return pages[k];
+//                }
+//            }
+//        }
+//    }
 }
 
 #endif //LRU_FINDLEAST_H
--- main.cpp
+++ main.cpp
@@ -6,20 +6,6 @@
 
 using namespace std;
 
-void sortFrequenciesByPage(vector<pair<int, int>> &frequencies) {
-    std::sort( frequencies.begin(), frequencies.end(),
-               [](const std::pair<int, int>& f, const std::pair<int, int>& s) {
-                   return f.first < s.first;
-               });
-}
-
-void sortFrequenciesByFreq(vector<pair<int, int>> &frequencies) {
-    std::sort( frequencies.begin(), frequencies.end(),
-               [](const std::pair<int, int>& f, const std::pair<int, int>& s) {
-                   return f.second < s.second;
-               });
-}
-
 int main()
 {
     int fault = 0;
@@ -29,29 +15,18 @@ int main()
     cout << "Storage capacity (number): ";
     cin >> capacity;
     vector<int> storage(capacity, -1);
+    vector<int> storageOrder(capacity, -1);
+    int order = 0;
 
-//    vector<int> pages = { 1, 2, 3, 4, 2, 1, 5 };
-//    vector<pair<int, bool>> removedPages = {
-//            {1, false},
-//            {2, false},
-//            {3, false},
-//            {4, false},
-//            {2, false},
-//            {1, false},
-//            {5, false}
-//    };
-    int pagesAmount = 3;
-    cout << "Pages amount (number): ";
-    cin >> pagesAmount;
-    cout << '\n';
-
-    cout << "----- generators -----\n";
-    vector<int> pages = generatePages(pagesAmount);
-    vector<pair<int, bool>> removedPages = generateRemoved(pages);
-    cout << "--- end of generators ---\n";
-
-
-    vector <pair<int,int>> frequencies;
+    vector<int> pages = { 7, 9, 1, 2, 9, 3, 9, 4, 2, 3, 9, 3, 2 };
+//    int pagesAmount = 3;
+//    cout << "Pages amount (number): ";
+//    cin >> pagesAmount;
+//    cout << '\n';
+//    cout << "----- generators -----\n";
+//    vector<int> pages = generatePages(pagesAmount);
+//    vector<pair<int, bool>> removedPages = generateRemoved(pages);
+//    cout << "--- end of generators ---\n";
 
     for(auto page: pages){
         cout << '\n';
@@ -60,93 +35,55 @@ int main()
 
         cout  << "Page " << page << '\n';
 
-        // Count/create Frequency
-        bool existFrequency = false;
-        for(int i=0 ; i<frequencies.size() ; i++){
-            if(frequencies[i].first == page) {
-                existFrequency = true;
-                frequencies[i].second++;
+        bool alreadyInStorage = false;
+        for(int i=0 ; i<storage.size() ; i++) {
+            if(storage[i] == page ) {
+                alreadyInStorage = true;
                 break;
             }
         }
-        if(!existFrequency) {
-            cout << "Making pair for page " << page << ": ";
-            frequencies.push_back(make_pair(page, 1));
-            cout << frequencies.back().first << ", " << frequencies.back().second << '\n';
-        }
 
-        sortFrequenciesByFreq(frequencies);
+        if(alreadyInStorage) {
+            cout << "Already in Storage...\n";
+            pageHit++;
 
-        // Fill Capacity blanks
-        bool seated = false;
-        for(int i=0 ; i<capacity ; i++) {
-            if(storage[i] == -1) {
-                fault++;
+            for(int j=0 ; j<storage.size() ; j++) {
+                if(storage[j] == page) {
+//                    cout << "Removing first instance of " << page << " that was at index " << j << '\n';
+//                    storage.erase(storage.begin() + j);
 
-                storage.at(i) = page;
-                seated = true;
-                break;
+                    cout << "Updating order of page " << page << '\n';
+                    storageOrder[j] = order++;
+                }
             }
-        }
-        //Compare frequencies
-        if(!seated) {
-            bool alreadyInStorage = false;
-            for(int i=0 ; i<storage.size() ; i++) {
-                if(page == storage[i]) {
-                    alreadyInStorage = true;
+        } else {
+            cout << "New in Storage...\n";
+            fault++;
+
+            int leastRecentStorageIndex = 0;
+            int min = storageOrder[0];
+            for(int j=0 ; j<storage.size() ; j++) {
+                if(storageOrder[j] < min) {
+//                    cout << "Removing first instance of " << page << " that was at index " << j << '\n';
+//                    storage.erase(storage.begin() + j);
+                    leastRecentStorageIndex = j;
+                        min = storageOrder[j];
                 }
             }
 
-            int temp; // page from 'storage' to be changed
-            if(alreadyInStorage) {
-                pageHit++;
-
-                std::vector<pair<int, int>>::iterator frequencyToIncrease;
-                frequencyToIncrease = find_if(frequencies.begin(), frequencies.end(), [page](pair<int, int> i) {
-                    return i.first == page;
-                });
-//                It is done before
-//                frequencyToIncrease->second++;
-                cout << "INCREASED frequencyToIncrease: { first: " << frequencyToIncrease->first << "  |  second: "
-                     << frequencyToIncrease->second << " } " << '\n';
-            } else {
-                fault++;
-
-                std::vector<pair<int, int>>::iterator frequencyToDecrease;
-                temp = findLeast(frequencies,pages, storage, removedPages);
-                cout << "For page -" << page << "- findLeast returned page " << temp << " from storage (to replace)\n";
-
-                frequencyToDecrease = find_if(frequencies.begin(), frequencies.end(), [temp](pair<int, int> i) {
-                    return i.first == temp;
-                });
-                frequencyToDecrease->second--;
-                cout << "DECREASED frequencyToDecrease: { first: " << frequencyToDecrease->first << "  |  second: "
-                << frequencyToDecrease->second << " } " << '\n';
-
-                for(int j=0 ; j<removedPages.size() ; j++) {
-                    if(removedPages[j].first == temp && removedPages[j].second == false) {
-                        cout << "(removedPages[" << j << "].first is " << removedPages[j].first << ") Removing page\n";
-                        removedPages[j].second = true;
-                        break;
-                    }
-                }
-            }
-
-            for (int i = 0; i < storage.size(); ++i) {
-                if(storage[i] == temp) {
-                    cout << "Storage change: " << storage[i] << " to " << page << '\n';
-                    storage[i] = page;
-                }
-            }
+            storage[leastRecentStorageIndex] = page;
+            storageOrder[leastRecentStorageIndex] = order++;
         }
+//        storage.push_back(page);
+
+//        if(storage.size() > capacity) {
+//            storage.erase(storage.begin());
+//        }
     }
 
     cout << '\n';
     displayStorage(storage);
     cout << '\n';
-
-    sortFrequenciesByPage(frequencies);
-    displayFrequencies(frequencies);
 
     cout << '\n';
     cout << "Page Faults = " << fault << '\n';
